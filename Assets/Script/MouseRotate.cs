b6c7ab8834c188689a069b90a373e5ca6294ced1
dinhhailinh <58936308+dinhhailinh@users.noreply.github.com>
2021-10-08 11:34:57 +0700

    DinhLinh
    fix smooth camera

--- Assets/Script/MouseRotate.cs
+++ Assets/Script/MouseRotate.cs
@@ -6,7 +6,7 @@ public class MouseRotate : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
     public Transform body;
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +20,11 @@ public class MouseRotate : MonoBehaviour
     }
     public void Rotate()
     {
+        
         float mouseX = Input.GetAxis("Mouse X")* mouseSensitivity * Time.deltaTime;
+        
 
         body.Rotate(Vector3.up * mouseX);
-
+       
     }
 }
